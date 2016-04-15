5d150938a8f57e1f8a8463db3eb17400ef64f121
Kryptogarten <kryptogarten@gmail.com>
2016-04-15 00:26:51 -0400

    Create des.h

---
+++ ciphers/des.h
@@ -0,0 +1,9 @@
+/*
+* File: des.h
+* Author: Julio Torres 
+* Date : April 16th 2016
+* 
+* Data encryption standard. It is present for historical reasons; it shouldn't ever be used for a production code. 
+* 
+* Source: 
+*/
